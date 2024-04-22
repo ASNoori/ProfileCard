cb2b09be772ccac779a73f87b014a3818a75a213
ASNoori <nooriameer12@gmail.com>
2024-04-23 00:20:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+       <Avatar/>
+       <div className="data">
+        <Intro/>
+        <SkillList/>
+       </div>
     </div>
   );
 }
 
-export default App;
+function Avatar(){
+  return <img className="avatar" src="laptop.jpg" alt="Laptop"/>
+}
+function Intro(){
+  return(
+    <div>
+      <h1>Noorinisha Begam</h1>
+      <p>
+        Full-stack web developer.I like to play board games, to cook (and eat), or to
+        just enjoy the sun at the beach.
+      </p>
+    </div>
+  )
+}
+const skilllist = [
+  {
+    skill:'react',
+    level:'intermediate',
+    color: 'blue'
+},
+{
+  skill:'html + css',
+  level:'advanced',
+  color: 'orange'
+},
+{
+skill:'JavaScript',
+level:'advanced',
+color: 'yellow'
+},
+{
+skill:'svelte',
+level:'beginner',
+color: 'orangered'
+}
+]
+function SkillList(){
+  return(
+    <div className='skill-list'>
+      {/* <Skill skill='React' emoji="💪" color='blue'/>
+      <Skill skill="HTML+CSS" emoji="💪" color="orange" />
+      <Skill skill="JavaScript" emoji="💪" color="yellow" />
+      <Skill skill="Svelte" emoji="👶" color="orangered" /> */}
+        { skilllist.map(lang=>
+          <Skill skills={lang} key={lang.skill}/>
+          )}
+    </div>
+  )
+}
+function Skill(props){
+  return(
+    <div className='skill' style={{backgroundColor:props.skills.color}}> 
+    {/* <span>{props.skill}</span>
+   <span>{props.emoji}</span> */}
+ 
+         <span>{props.skills.skill}</span>
+      <span>{props.skills.level==='advanced'?'💪': props.skills.level ==='intermediate' ? '🧩' : '🔰' }</span>
+      </div>
+      )
+
+}
